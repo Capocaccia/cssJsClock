c9a3c0b5bfbe9f950467eda589aa785d8a3cf6fe
Carter Capocaccia <carter.capocaccia@gmail.com>
2017-11-06 20:51:27 -0600

    adds function to get seconds minutes and hours hands moving

--- js/clock.js
+++ js/clock.js
@@ -4,14 +4,17 @@ const hoursHand = document.querySelector('.hour-hand');
 
 function setDate(){
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
     var secondsDegrees = ((seconds / 60) * 360) + 90;
-    var minutesDegrees = ((minutes / 60) * 360) + 90;
-    var hoursDegrees = ((hours / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    var minutesDegrees = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    var hoursDegrees = ((hours / 12) * 360) + 90;
     hoursHand.style.transform = `rotate(${hoursDegrees}deg)`
 }
 
